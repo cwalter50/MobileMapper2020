795306b1d1190e6aeb73f31df189894bd037a49b
Chris Walter <walt50@gmail.com>
2020-11-21 12:35:32 -0500

    Initial Commit

---
+++ MobileMapper2020/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileMapper2020
+//
+//  Created by Christopher Walter on 11/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
