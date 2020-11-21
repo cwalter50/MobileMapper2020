e3895810547ce20b257f70e368059b69fcbc30a1
Chris Walter <walt50@gmail.com>
2020-11-21 13:23:06 -0500

    location manager tracking current location

--- MobileMapper2020/ViewController.swift
+++ MobileMapper2020/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController, CLLocationManagerDelegate
 {
     
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager = CLLocationManager()
+    
+    var currentLocation = CLLocation()
     
     override func viewDidLoad()
     {
@@ -22,8 +24,15 @@ class ViewController: UIViewController
         locationManager.requestWhenInUseAuthorization()
         mapView.showsUserLocation = true
         
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
         
-        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+        currentLocation = locations[0]
     }
 
 
